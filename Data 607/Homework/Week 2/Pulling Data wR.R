ed0e461f92ee90aedd891dc9ecd68a40d28c99d2
() () () <chris.edouard.hunt@gmail.com>
2016-02-07 11:09:50 -0500

    update

--- Data 607/Homework/Week 2/Pulling Data wR.R
+++ Data 607/Homework/Week 2/Pulling Data wR.R
@@ -1,17 +1,24 @@
-f.data <- file("https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data",
+library(tm)
+
+f.data <- file("http://archive.ics.uci.edu/ml/machine-learning-databases/flags/flag.data",
                open="r" )
 
 data <- read.table(f.data, 
-                   sep="", 
+                   sep=",", 
                    header=FALSE, 
                    stringsAsFactors = FALSE)
 
-newdata <- data[1:6]
+newdata <- data[, c( 1,2,3,6,7)]
 
 f.data.dictionary <- file("https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.names", 
                           open="r" )
+
+test <- readLines("http://archive.ics.uci.edu/ml/machine-learning-databases/flags/flag.names")
+docs <- Corpus(VectorSource(test))
 
 data.dictionary <- read.table(f.data.dictionary, 
                               sep="", 
                               header=FALSE, 
                               stringsAsFactors = FALSE)
+
+dataframe <- data.frame(text=unlist(sapply(docs, `[`, "content")), stringsAsFactors=F)
