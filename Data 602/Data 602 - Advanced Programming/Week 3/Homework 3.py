e334dd7a317decd2f2b9cbf335a001367ab9b6db
() () () <chris.edouard.hunt@gmail.com>
2016-02-17 22:17:26 -0500

    update

--- Data 602/Data 602 - Advanced Programming/Week 3/Homework 3.py
+++ Data 602/Data 602 - Advanced Programming/Week 3/Homework 3.py
@@ -1,8 +1,4 @@
 
-# coding: utf-8
-
-# In[ ]:
-
 import requests
 import csv
 import pandas 
@@ -33,7 +29,8 @@ elif s.lower() == 'n':
     root.withdraw()
     
 else: 
-    print "input was not in a y/n format"
+    print "input was not in y/n format"
+    sys.exit("End")
 
 df.columns = [ 'buying', 'maint', 'doors','persons','lug_boot', 'safety', 'class']
 df = df.apply(lambda df: df.astype('category')) 
@@ -43,22 +40,18 @@ maint_cat = ['low','med','high','vhigh']
 doors_cat = ['2','3','4','5more']
 
 
-# In[ ]:
-
 def columncheck(column, listvalues):
     check = list(set(pandas.unique(df[column].ravel())) - set(listvalues))
     if check != []:
        s = raw_input("%s column contains unexpected values:%s, this will be converted to NaN. Continue? y/n : " 
                      % (column, check))
        if s.lower() != 'y':
-          sys.exit("End") 
+          sys.exit("End")
 
 columncheck('safety', safety_cat)
 columncheck('maint', maint_cat)
 columncheck('doors', doors_cat)
 
-
-# In[ ]:
 
 def setcat(column, listvalues):
     df[column].cat.set_categories(listvalues, inplace = True)
@@ -67,51 +60,27 @@ setcat('safety', safety_cat)
 setcat('maint', maint_cat)
 setcat('doors', doors_cat)
 
-
-# In[ ]:
-
 #Print to the console the top 10 rows of the data sorted by 'safety' in descending order
 results = df.sort_values(by = 'safety', ascending=[1])
 print results.head(10)
-
-
-# In[ ]:
-
 raw_input("Press Enter to continue...  ")
-
-
-# In[ ]:
 
 #Print to the console the bottom 15 rows of the data sorted by 'maint' in ascending order
 results = df.sort_values(by = 'maint', ascending = [0])
 print results.tail(15)
-
-
-# In[ ]:
-
 raw_input("Press Enter to continue...  ")
 
-
-# In[ ]:
-
-#Print to the console all rows that are high or vhigh in fields 'buying', 'maint', and 'safety', 
+#Print to the console all rows that are high or vhigh in fields 'buying', 'maint', and 'safety',
 # sorted by 'doors' in ascending order.  Find these matches using regular expressions.
 mask = df[['buying', 'maint','safety']].apply(lambda column: column.str.contains('vhigh|high', regex=True)).any(axis=1)
 results = df[mask].sort_values(by = 'doors', ascending = [1])
 print results
-
-
-# In[ ]:
-
 raw_input("Press Enter to continue...  ")
-
-
-# In[ ]:
 
 #Save to a file all rows (in any order) that are: 'buying': vhigh, 'maint': med, 'doors': 4, and 
 #'persons': 4 or more.  The file path can be a hard-coded location (name it output.txt) or use a dialog box.  
 results = df.query("buying == 'vhigh' and maint == 'med' and doors == '4' and (persons == '4' or persons == 'more')")
+print results
 savefile = tkFileDialog.asksaveasfile(mode='w', defaultextension=".csv")
 results.to_csv(savefile, index = False)
 savefile.close()
-
