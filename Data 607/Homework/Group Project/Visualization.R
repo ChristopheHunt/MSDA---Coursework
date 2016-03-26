286fe4a8bead7466b47a822ef4a2009fd115b5ed
() () () <chris.edouard.hunt@gmail.com>
2016-03-26 12:08:07 -0400

    update

--- Data 607/Homework/Group Project/Visualization.R
+++ Data 607/Homework/Group Project/Visualization.R
@@ -1,8 +1,16 @@
 library(RColorBrewer)
+library(ggplot2)
+library(RMySQL)
 
-data<- read.csv("https://raw.githubusercontent.com/ChristopheHunt/MSDA---Coursework/master/Data%20607/Homework/Group%20Project/tbl_data_version1%20.csv")
+proj_user <- "project3"
+proj_pwd  <- "CUNYRBridge4"
+proj_db   <- "skill"
+proj_host <- "db4free.net"
 
-ggplot(data, aes(source_name, skill_name, label = skill_name, 
+skilldb = dbConnect(MySQL(), user=proj_user, password=proj_pwd, dbname=proj_db, host=proj_host)
+data <- dbGetQuery(skilldb, "select * from tbl_data")
+
+ggplot(data[(data$source_id != 2),], aes(source_name, skill_name, label = skill_name, 
                     size = weighted_rating_overall, fill = skill_type_name)) + 
         geom_point(pch = 21) + 
         scale_fill_manual(values =  brewer.pal(9, "Set1")) + 
