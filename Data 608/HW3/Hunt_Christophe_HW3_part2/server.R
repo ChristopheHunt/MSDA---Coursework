05bdf2f024cd5133c2fe410cc8d380fdc92ac2a8
Christophe Hunt <chris.edouard.hunt@gmail.com>
2016-10-30 21:22:46 -0400

    update

--- Data 608/HW3/Hunt_Christophe_HW3_part2/server.R
+++ Data 608/HW3/Hunt_Christophe_HW3_part2/server.R
@@ -2,34 +2,72 @@ dfMort <- read.csv("https://raw.githubusercontent.com/ChristopheHunt/MSDA---Cour
 #
 #
 library(pacman)
-p_load(shiny, tidyverse, plotly, forcats, plotly)
-
-dfMort <- dfMort %>%
-  filter(Year.Code == 2010) %>%
-  mutate(Crude.Rate = round(((Deaths/Population)*100000),1)) %>%
-  select(State, ICD.Chapter, Crude.Rate) 
+p_load(shiny, tidyverse, plotly, forcats, plotly, scales)
 
 shinyServer(function(input, output) {
   
-  output$distPlot <- renderPlotly({
+  output$distPlot <- renderPlot({
     
-    dfMortSub <- dfMort %>% 
-      filter(ICD.Chapter %in% input$conditions) %>%
-      group_by(State) %>%
-      summarise(Crude.Rate = sum(Crude.Rate)) %>%
-      arrange(Crude.Rate) %>%
-      mutate(State = factor(State,State))
+    dfUs <- dfMort %>%
+            mutate(State = "All States") %>%
+            filter(Year == as.integer(
+              input$timePeriod[[1]]
+              #1999 # test value 
+              ) | Year == as.integer(
+              input$timePeriod[[2]]
+              #2010 # test value  
+              )) %>%
+            group_by(State, ICD.Chapter, Year) %>%
+            select(State, ICD.Chapter, Year, Deaths, Population) %>%
+            summarise(Deaths = sum(as.numeric(Deaths)), 
+                      Population = sum(as.numeric(Population))) %>%
+            mutate(Crude.Rate = round(((Deaths/Population)*100000),1)) %>%
+            select(State, ICD.Chapter, Year, Crude.Rate)  %>% 
+            filter(ICD.Chapter %in% 
+                       input$conditions
+                       #c("Certain infectious and parasitic diseases") # test value
+              ) %>%
+           spread(Year, Crude.Rate) %>%
+           ungroup() %>%
+           mutate(`% Change` = ((.[[4]] - .[[3]])/.[[3]]))
+
     
-    ggplot(data = dfMortSub, aes(y = Crude.Rate, x = State, color = Crude.Rate)) +
-      scale_color_distiller(palette = "YlOrRd") +
+    dfMortSub <- dfMort %>%
+      filter(Year == as.integer(
+        input$timePeriod[[1]]
+        #1999 # test value 
+      ) | Year == as.integer(
+        input$timePeriod[[2]]
+        #2010 # test value  
+      )) %>%
+      mutate(Crude.Rate = round(((Deaths/Population)*100000),1)) %>%
+      select(State, ICD.Chapter, Year, Crude.Rate)  %>% 
+      filter(ICD.Chapter %in% 
+               input$conditions
+               #c("Certain infectious and parasitic diseases") #test value
+      )  %>%
+      group_by(State, ICD.Chapter, Year) %>%
+      spread(Year, Crude.Rate) %>%
+      ungroup() %>%
+      mutate(`% Change` = ((.[[4]] - .[[3]])/.[[3]])) %>%
+      arrange(-`% Change`) %>%
+      mutate(State = factor(State,State)) 
+      
+    
+    ggplot(data = dfMortSub, aes(y = `% Change`, x = State, fill = `% Change`)) +
       geom_bar(stat = "identity", width = .5) +
-      ylab("Rate (per 100000 people)") + 
+      scale_fill_distiller(palette = "YlOrRd", label = percent, name = "% Change \n") + 
+      scale_y_continuous(labels = percent) + 
+      ylab(paste("Percentage Change of Mortality Rate from Year", input$timePeriod[[1]], "to",input$timePeriod[[2]])) + 
+      geom_hline(aes(yintercept = dfUs$`% Change`, linetype = dfUs$State), size = .75) +
+      scale_linetype_discrete("National Rate Change") + 
       coord_flip() + 
-      theme_minimal()
+      theme_minimal() 
+      
   })
   
-  output$event <- renderPrint({
-    d <- event_data("plotly_hover")
-  })
+ # output$event <- renderPrint({
+ #  d <- event_data("plotly_hover")
+ # })
   
 })
--- Data 608/HW3/Hunt_Christophe_HW3_part2/ui.R
+++ Data 608/HW3/Hunt_Christophe_HW3_part2/ui.R
@@ -1,6 +1,5 @@
 #Often you are asked whether particular States are improving their mortality rates (per cause) faster than, or slower than, the national average. Create a visualization that lets your clients see this for themselves for one cause of death at the time. Keep in mind that the national average should be weighted by the national population.
 
-
 dfMort <- read.csv("https://raw.githubusercontent.com/ChristopheHunt/MSDA---Coursework/master/Data%20608/HW3/cleaned-cdc-mortality-1999-2010.csv")
 
 library(shiny)
@@ -17,18 +16,17 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      sliderInput("foo", "Time Period", min = min(as.numeric(dfMort$Year)),
-                  max = max(as.numeric(dfMort$Year)), value = c(min(as.numeric(dfMort$Year)), max(as.numeric(dfMort$Year))), sep = ""),
-     
+      sliderInput("timePeriod", "Time Period", min = min(as.numeric(dfMort$Year)),
+                  max = max(as.numeric(dfMort$Year)), 
+                  value = c(min(as.integer(dfMort$Year)), max(as.numeric(dfMort$Year))), sep = ""),
       selectInput("conditions", "Conditions", 
                   unique(as.character(dfMort$ICD.Chapter)), selectize = TRUE),
       tableOutput("data")
-      
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotlyOutput("distPlot", height = 700, width = 500)
+      plotOutput("distPlot", height = 700, width = 500)
       
     )
   )
